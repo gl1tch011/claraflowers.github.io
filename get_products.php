877e03897d17a5bd45a6f20d94a866c9575366f6
Babək Rəsulov <109175939+gl1tch011@users.noreply.github.com>
2025-02-18 17:06:16 +0400

    Update get_products.php

--- get_products.php
+++ get_products.php
@@ -1,13 +1,1 @@
-<?php
-$conn = new mysqli("localhost", "root", "", "claraflowers");
 
-$sql = "SELECT * FROM flowers ORDER BY created_at DESC";
-$result = $conn->query($sql);
-
-$products = array();
-while($row = $result->fetch_assoc()) {
-    $products[] = $row;
-}
-
-echo json_encode($products);
-?>
