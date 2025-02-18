0280088ca7a090fea4a4f9afcb5db70fa9b102fa
Babək Rəsulov <109175939+gl1tch011@users.noreply.github.com>
2025-02-18 17:06:03 +0400

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,13 +1,1 @@
-CREATE DATABASE claraflowers;
 
-USE claraflowers;
-
-CREATE TABLE flowers (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255),
-    price DECIMAL(10,2),
-    description TEXT,
-    image VARCHAR(255),
-    seller VARCHAR(255),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
